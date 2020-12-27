692d40bc54f8a76b0e37a4cf6cbc85376fd3dc98
LiloviaLilossy <55652497+LiloviaLilossy@users.noreply.github.com>
2020-12-27 18:09:40 +0300

    [cogs/python.py] Setting is_owner decorator
    It's better to not leave eval/exec commands open for everyone. Unless you want for users to leak your bot token or break things using your bot.

--- cogs/python.py
+++ cogs/python.py
@@ -5,10 +5,12 @@ class Python(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.is_owner()
     @commands.command(name='eval')
     async def eval(self, ctx, *, cmd: str):
         await ctx.send('{0}'.format(eval(cmd)))
 
+    @commands.is_owner()
     @commands.command(name='exec')
     async def exec(self, ctx, *, cmd: str):
         await ctx.send('{0}'.format(exec(cmd)))
